2aac22238c5505fbbe1eb4facd42bd651872b4e9
Aleksandrov Maksim <aleksandrovmagit@gmail.com>
2024-07-23 21:48:14 +0400

    Modif JavaConfig Описание изменений: 	В JavaConfig c помощью Reflections ищем все реализации интерфейса 	и если всего одна реализация, то возвращаем отот класс-реализацию 	, иначе выбрасывается RuntimeException что 0 или есть более 1 	реализации.
    Новые классы:
    Измененные классы:
    -	JavaConfig
    Удаленные классы:
    
    Файлы:

--- src/main/java/com/mycompany/myownspringwithborisove/JavaConfig.java
+++ src/main/java/com/mycompany/myownspringwithborisove/JavaConfig.java
@@ -1,12 +1,26 @@
 package com.mycompany.myownspringwithborisove;
 
+import java.util.Set;
+import org.reflections.Reflections;
+
 
 
 public class JavaConfig implements Config{
 
+    private Reflections scanner;
+
+    public JavaConfig(String packageToScan) {
+        this.scanner = new Reflections(packageToScan);
+    }
+    
     @Override
     public <T> Class<? extends T> getImplClass(Class<T> infc) {
-        return null;
+        Set<Class<? extends T>> classes =
+                scanner.getSubTypesOf(infc);
+        if(classes.size() != 1){
+            throw new RuntimeException(infc + " has 0 or more then one impl");
+        }
+        return classes.iterator().next();
     }
     
 }
