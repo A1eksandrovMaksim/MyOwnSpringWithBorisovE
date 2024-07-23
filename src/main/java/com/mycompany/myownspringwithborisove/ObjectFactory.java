034f762f096eaf55444e6faf253bac527cfe9b1e
Aleksandrov Maksim <aleksandrovmagit@gmail.com>
2024-07-23 23:17:34 +0400

    Add config property before return Impl Описание изменений: 	Была добавлена настройка значений объекта перед его возвращением 	из метода createObject в ObjectFactory. Если значение 	аннотровано InjectProperty. Значение ищется в application.yml.
    Новые классы:
    Измененные классы:
    -	ObjectFactory
    Удаленные классы:
    
    Файлы:

--- src/main/java/com/mycompany/myownspringwithborisove/ObjectFactory.java
+++ src/main/java/com/mycompany/myownspringwithborisove/ObjectFactory.java
@@ -1,7 +1,11 @@
 package com.mycompany.myownspringwithborisove;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import lombok.SneakyThrows;
 
 
@@ -29,7 +33,32 @@ public class ObjectFactory {
         }
         T t = implClass.getDeclaredConstructor().newInstance();
         
-        // todo
+        for(var field : implClass.getDeclaredFields()){
+            InjectProperty annotation = 
+                    field.getAnnotation(InjectProperty.class);
+            
+            String path = ClassLoader.getSystemClassLoader()
+                            .getResource("application.yml")
+                            .getPath();
+            Stream<String> lines = 
+                    new BufferedReader(
+                            new FileReader(path)).lines();
+            Map<String, String> propertiesMap = 
+                    lines.map(line->line.split(": "))
+                    .collect(Collectors.toMap(arr->arr[0], arr->arr[1]));
+            
+            
+            if(annotation != null){
+                String value;
+                if(annotation.value().isEmpty()){
+                    value = propertiesMap.get(field.getName());
+                }else{
+                    value = propertiesMap.get(annotation.value());
+                }
+                field.setAccessible(true);
+                field.set(t, value);
+            }
+        }
         
         return t;
     }
