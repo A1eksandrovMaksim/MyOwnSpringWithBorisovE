90e93adcda56637a53d195cbe6a2f8cd898620de
Aleksandrov Maksim <aleksandrovmagit@gmail.com>
2024-07-23 21:56:42 +0400

    Modif ObjectFactory createObject Описание изменений: 	Дописал метод createObject. 	Он проверяет, евляется ли переданный класс интерфейсом, если да, 	то просим Config найти класс, реализующий его, и через рефлексию 	создаем новый объект этого класса и возвращаем его.
    Новые классы:
    Измененные классы:
    -	ObjectFactory
    Удаленные классы:
    
    Файлы:

--- src/main/java/com/mycompany/myownspringwithborisove/ObjectFactory.java
+++ src/main/java/com/mycompany/myownspringwithborisove/ObjectFactory.java
@@ -1,12 +1,15 @@
 package com.mycompany.myownspringwithborisove;
 
+import lombok.SneakyThrows;
+
 
 
 public class ObjectFactory {
     
     private static ObjectFactory instance = new ObjectFactory();
       
-    private Config config;
+    private Config config = 
+            new JavaConfig("com.mycompany.myownspringwithborisove");
             
     public static ObjectFactory getInstance(){
         return instance;
@@ -15,11 +18,17 @@ public class ObjectFactory {
     private ObjectFactory(){
     }
     
+    @SneakyThrows
     public <T> T createObject(Class<T> type){
         Class<? extends T> implClass = type;
         if(type.isInterface()){
             implClass = config.getImplClass(type);
         }
+        T t = implClass.getDeclaredConstructor().newInstance();
+        
+        // todo
+        
+        return t;
     }
     
 }
